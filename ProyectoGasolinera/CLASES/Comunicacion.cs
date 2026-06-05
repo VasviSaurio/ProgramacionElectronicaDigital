66c9bf855f3620bbd02a098cbb6c37ee6afa61c3
Jos3milio <sajgar2084@umes.edu.gt>
2026-06-05 00:00:27 -0600

    avanceClases2

---
+++ ProyectoGasolinera/CLASES/Comunicacion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoGasolinera.CLASES
+{
+    internal class Comunicacion
+    {
+        public string Puerto { get; set; }
+
+        public Comunicacion(string puerto)
+        {
+            Puerto = puerto;
+        }
+
+        public void EnviarMensajeJSON(string mensaje)
+        {
+            Console.WriteLine($"Enviando mensaje JSON por {Puerto}: {mensaje}");
+        }
+
+        public string RecibirMensajeJSON()
+        {
+            string mensaje = "{ \"estado\": \"finalizado\" }";
+            Console.WriteLine($"Recibiendo mensaje JSON por {Puerto}: {mensaje}");
+            return mensaje;
+        }
+    }
